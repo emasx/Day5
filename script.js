428eb7391870c7ffd9ff5a50ee5b2a9a27dc080a
emasx <29961897+emasx@users.noreply.github.com>
2022-08-18 23:54:46 +0300

    Operator Precedence

---
+++ script.js
@@ -0,0 +1,11 @@
+const now = 2037;
+const ageEmas = now - 1994;
+const ageMaria = now - 2011;
+
+console.log(now - 1994 > now - 2011);
+
+let x, y;
+
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
